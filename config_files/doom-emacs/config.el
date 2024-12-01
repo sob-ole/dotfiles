d654b382069fd249f1b8fbcd4ed62946d084420f
Sobolev Oleg <SobolevOleg@yandex.ru>
2024-12-01 10:23:36 +0700

    Enabled multiple cursors + debugger, fixed theme loading

--- config_files/doom-emacs/config.el
+++ config_files/doom-emacs/config.el
@@ -1,43 +1,12 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-
-
-
-
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-
 ;; Установка шрифта
 (setq doom-font (font-spec :family "DejaVu Sans Mono" :size 16))
 (setq doom-variable-pitch-font (font-spec :family "DejaVu Sans Mono" :size 16))
 
-(setq doom-theme 'doom-gruvbox)
+;; Настройка темы
+(setq custom-safe-themes t)
+(disable-theme 'default)
+(load-theme 'doom-gruvbox t)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -70,7 +39,6 @@
   (c-set-offset 'statement-cont 0))
 
 (add-hook 'c-mode-common-hook #'my-c-mode-common-hook)
-
 
 (defun my-sh-mode-common-hook ()
   "Custom settings for Bash programming."
@@ -105,8 +73,6 @@
 ;; Treemacs - проектный файловый обзорщик в левой половине окна
 (use-package! treemacs)
 (treemacs-follow-mode)
-;;(treemacs-indent-guide-mode)
-;; (setq treemacs-fringe-indicator-mode 'always)
 
 (use-package! tree-sitter)
 ;; Глобальная подсветка синтаксиса
@@ -118,41 +84,34 @@
 
 (add-hook 'tree-sitter-after-on-hook #'my-enable-tree-sitter-hl-mode)
 
-
 (use-package! flycheck-plantuml)
 ;; Диаграммы Plantuml
 (with-eval-after-load 'flycheck
   (require 'flycheck-plantuml)
   (flycheck-plantuml-setup))
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+(setq plantuml-output-type "svg")
+
+(map! :n "C-/" #'comment-line) ; Normal mode
+(map! :i "C-/" #'comment-line) ; Insert mode
+(map! :v "C-/" #'comment-line) ; Visual mode
+
+;; Настройки mode-line
+(setq doom-modeline-vcs-max-length 40);; Название git-ветки
+
+;; Настройки часов
+;; Не оповещять о новых письмах (убирает лишнюю точку *)
+(setq display-time-mail-directory nil) ;; Не проверять почту
+(setq display-time-format "%H:%M")
+(setq display-time-mail-function nil)
+(display-time-mode 1)
+
+;; ;; Отрисовка пробелов
+;; (add-hook 'prog-mode-hook #'whitespace-mode)
+;; (setq whitespace-style '(face tabs spaces trailing))
+;; (add-hook 'completion-list-mode-hook (lambda () (whitespace-mode -1)))
+;; (add-hook 'help-mode-hook (lambda () (whitespace-mode -1)))
+;; (add-hook 'special-mode-hook (lambda () (whitespace-mode -1)))
+;; (set-face-attribute 'whitespace-tab nil :background "gray20" :foreground "gray40")
+;; (set-face-attribute 'whitespace-space nil :background nil :foreground "gray30")
+;; (set-face-attribute 'whitespace-trailing nil :background "red1" :foreground nil)
